9a07da305de4039b2360fa6c3635eb6131241202
Gonzalo Da Silva <gonzalomauriciodt@gmail.com>
2026-05-24 12:56:56 -0500

    add @vercel/react-router preset, simplify vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { vercelPreset } from "@vercel/react-router/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [reactRouter()],
+  plugins: [reactRouter({ presets: [vercelPreset()] })],
 });
